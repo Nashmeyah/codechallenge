08e262ba913ef9a9a9158ae3b704443617d49219
Nashmeyah <nashalrekabi@gmail.com>
2021-02-18 20:28:08 -0700

    creating disabled submit button

--- index.js
+++ index.js
@@ -2,9 +2,5 @@ function checkform() {
   var f = document.forms["form"].elements;
   var cansubmit = true;
 
-  if (cansubmit) {
-    document.getElementById("submitbutton").disabled = false;
-  } else {
-    document.getElementById("submitbutton").disabled = "disabled";
-  }
+  document.getElementById("submitbutton").disabled = !cansubmit;
 }
